a2f29c9c64e0b80d7062e7eb568520c380ecbf81
Umut-2647 <23010903072@subu.edu.tr>
2023-11-22 17:29:22 +0300

    first

--- degiskenler.py
+++ degiskenler.py
@@ -1,5 +1,10 @@
+# degişken tanımlanırken değişken türü belirtilmez
+
+
 maasumut =5000
 maasyasin =4000
 vergi =0.27
+
 print(maasumut-(maasumut*vergi))
 print(maasyasin-(maasyasin*vergi))
+print("selam")
